7f9d80c0c66a437bd4cbaf955d0adf5437ae472c
AdiNehama <adinehama6@gmail.com>
2025-01-01 11:07:02 +0200

    start work

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  
+      <h1>עירית רן כהן</h1>
+     
+      <p className="read-the-docs">
+אתר בעדכון      </p>
+    </>
+  )
+}
+
+export default App
